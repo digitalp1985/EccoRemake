3949b24517d1c1269f01b412f002948b3fb73716
Digitalp2k <digitalp1985@gmail.com>
2020-10-14 13:59:59 -0700

    Massive updates

---
+++ Assets/Scripts/DebugDevelopment.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugDevelopment : MonoBehaviour
+{
+    float currentVelocity;
+    public Rigidbody2D characterrb;
+    public Text displayText;
+
+    void getVelocity()
+    {
+        currentVelocity = characterrb.velocity.magnitude;
+    }
+
+    void displayVelocity()
+    {
+        string tempstr;
+        tempstr = currentVelocity.ToString();
+        displayText.text = "Current Speed = " + tempstr;
+    }
+
+    private void Update()
+    {
+        getVelocity();
+        displayVelocity();
+    }
+}
